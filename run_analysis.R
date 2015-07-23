b4c949c84eaaf003840e76cd982cdaf6871d2b4f
John Oxley <john.oxley@liquidtelecom.com>
2015-07-23 17:00:19 +0100

    tweaked wanted features

--- run_analysis.R
+++ run_analysis.R
@@ -15,31 +15,36 @@ features <-
              FALSE)
 
 # These are the features we want to report on
-wanted_features <- features %>% filter(func %in% c("mean", "std"))
+wanted_features <- features %>% 
+  filter(func %in% c("mean", "std")) %>%
+  mutate(colname=gsub("(-|\\(|\\))", ".", label))
 
 load.dataset <- function(set) {
-  subjects <- read.table(
-    paste("UCI HAR Dataset/", set, "/subject_", set, ".txt", sep = ""), col.names =
-      "subject"
-  )
+  subjects <- read.table(paste("UCI HAR Dataset/", set, "/subject_", set, ".txt", sep = ""), col.names =
+                           "subject")
   
   loaded_activities <-
     read.table(paste("UCI HAR Dataset/", set, "/y_", set, ".txt", sep = ""), col.names =
-                 "activity_id") %>% left_join(activities, by = "activity_id")
+                 "activity_id") %>%
+    left_join(activities, by = "activity_id") %>%
+    select(-activity_id)
   
   measurements <-
-    read.table(paste("UCI HAR Dataset/", set, "/X_", set, ".txt", sep = ""), col.names = features$label) %>% 
+    read.table(paste("UCI HAR Dataset/", set, "/X_", set, ".txt", sep = ""), col.names = features$label) %>%
     select(wanted_features$feature_id)
   
   dataset <-
     tbl_df(data.frame(subjects, loaded_activities, measurements))
   
-  dataset
+  dataset %>%
+    group_by(subject, activity)
 }
 
 load.datasets <- function() {
   bind_rows(load.dataset("train"), load.dataset("test"))
 }
 
-# expect mean(dataset$tBodyAcc.mean...X[dataset$subject==1 & dataset$activity_id ==6])
+# expect 
+# mean(dataset$tBodyAcc.mean...X[dataset$subject==1 & dataset$activity_id ==6])
+# mean(dataset$tBodyAcc.mean...X[dataset$subject==1 & dataset$activity == "LAYING"])
 # 0.2215982
